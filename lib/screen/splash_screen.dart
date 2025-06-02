e44379fcc82e6b5abb194a5a41bcc09d5d47ac38
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2025-06-02 18:33:35 +0300

    Update splash_screen.dart

--- lib/screen/splash_screen.dart
+++ lib/screen/splash_screen.dart
@@ -1,5 +1,7 @@
+import 'package:ai_assistant/helper/pref.dart';
 import 'package:flutter/material.dart';
 import 'package:ai_assistant/helper/global.dart';
+import 'package:ai_assistant/screen/home_screen.dart';
 import 'package:ai_assistant/widget/custom_loading.dart';
 import 'package:ai_assistant/screen/onboarding_screen.dart';
 
@@ -15,9 +17,12 @@ class _SplashScreenState extends State<SplashScreen> {
   void initState() {
     super.initState();
     Future.delayed(const Duration(seconds: 2), () {
-      Navigator.of(
-        context,
-      ).pushReplacement(MaterialPageRoute(builder: (_) => OnboardingScreen()));
+      Navigator.of(context).pushReplacement(
+        MaterialPageRoute(
+          builder:
+              (_) => Pref.showOnboarding ? OnboardingScreen() : HomeScreen(),
+        ),
+      );
     });
   }
 
