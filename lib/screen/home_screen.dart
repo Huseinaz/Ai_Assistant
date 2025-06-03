73494cb37530889940037ae7479b24150755b997
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2025-06-03 15:19:00 +0300

    Update home_screen.dart

--- lib/screen/home_screen.dart
+++ lib/screen/home_screen.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:ai_assistant/helper/pref.dart';
 import 'package:ai_assistant/helper/global.dart';
+import 'package:ai_assistant/model/home_type.dart';
+import 'package:ai_assistant/widget/home_card.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -24,6 +26,41 @@ class _HomeScreenState extends State<HomeScreen> {
     // Initializing device size
     mq = MediaQuery.sizeOf(context);
 
-    return Scaffold(body: Center(child: Text('Welcome to Home Screen!')));
+    return Scaffold(
+      // AppBar
+      appBar: AppBar(
+        elevation: 0,
+        centerTitle: true,
+        title: Text(
+          appName,
+          style: TextStyle(
+            fontSize: 20,
+            color: Colors.blue,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        actions: [
+          IconButton(
+            padding: EdgeInsets.only(right: 10),
+            icon: Icon(
+              Icons.brightness_4_rounded,
+              color: Colors.blue,
+              size: 26,
+            ),
+            onPressed: () {},
+          ),
+        ],
+        backgroundColor: Colors.white,
+      ),
+
+      // Body
+      body: ListView(
+        padding: EdgeInsets.symmetric(
+          horizontal: mq.width * 0.04,
+          vertical: mq.height * 0.015,
+        ),
+        children: HomeType.values.map((e) => HomeCard(homeType: e)).toList(),
+      ),
+    );
   }
 }
