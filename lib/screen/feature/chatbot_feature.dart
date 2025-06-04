981bbe505e3a188b49c5280b7e46a40b296ac674
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2025-06-04 16:28:31 +0300

    Create chatbot_feature.dart

---
+++ lib/screen/feature/chatbot_feature.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:ai_assistant/helper/global.dart';
+import 'package:ai_assistant/widget/message_card.dart';
+import 'package:ai_assistant/controller/chat_controller.dart';
+import 'package:get/get_state_manager/get_state_manager.dart';
+
+class ChatBotFeature extends StatefulWidget {
+  const ChatBotFeature({super.key});
+
+  @override
+  State<ChatBotFeature> createState() => _ChatBotFeatureState();
+}
+
+class _ChatBotFeatureState extends State<ChatBotFeature> {
+  final _c = ChatController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      // AppBar
+      appBar: AppBar(title: const Text('Chat with AI Assistant')),
+
+      // Send message field and button
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8),
+        child: Row(
+          children: [
+            // Text input field
+            Expanded(
+              child: TextFormField(
+                controller: _c.textC,
+                onTapOutside: (e) => FocusScope.of(context).unfocus(),
+                decoration: InputDecoration(
+                  fillColor: Colors.white,
+                  filled: true,
+                  isDense: true,
+                  hintText: 'Ask me anything you want...',
+                  hintStyle: TextStyle(fontSize: 14),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(50)),
+                  ),
+                ),
+              ),
+            ),
+
+            const SizedBox(width: 8),
+
+            // Send button
+            CircleAvatar(
+              radius: 24,
+              backgroundColor: Colors.blue,
+              child: IconButton(
+                onPressed: _c.askQuestion,
+                icon: Icon(Icons.rocket_launch, color: Colors.white, size: 28),
+              ),
+            ),
+          ],
+        ),
+      ),
+
+      // Body
+      body: Obx(
+        () => ListView(
+          physics: const BouncingScrollPhysics(),
+          padding: EdgeInsets.only(
+            top: mq.height * 0.02,
+            bottom: mq.height * 0.1,
+          ),
+          children: _c.list.map((e) => MessageCard(message: e)).toList(),
+        ),
+      ),
+    );
+  }
+}
