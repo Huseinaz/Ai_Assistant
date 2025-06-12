126715ebb45e62eff8c3e6ad05af516ac8003757
Hussein Abou Zeinab <husseinabouzeinab@gmail.com>
2025-06-12 13:48:19 +0300

    Add dark mode theme

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:ai_assistant/helper/pref.dart';
 import 'package:ai_assistant/helper/global.dart';
@@ -29,6 +29,19 @@ class MyApp extends StatelessWidget {
       title: appName,
       debugShowCheckedModeBanner: false,
 
+      themeMode: Pref.defaultTheme,
+
+      // dark
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        appBarTheme: AppBarTheme(
+          elevation: 1,
+          centerTitle: true,
+          titleTextStyle: TextStyle(fontSize: 20, fontWeight: FontWeight.w500),
+        ),
+      ),
+
+      // light
       theme: ThemeData(
         appBarTheme: AppBarTheme(
           elevation: 1,
@@ -46,4 +59,19 @@ class MyApp extends StatelessWidget {
       home: SplashScreen(),
     );
   }
+}
+
+extension AppTheme on ThemeData {
+  // Light text color
+  Color get lightTextColor =>
+      brightness == Brightness.dark ? Colors.white70 : Colors.black54;
+
+  Color get lightTextColor2 =>
+      brightness == Brightness.dark ? Colors.white : Colors.black;
+
+  // Button color
+  Color get buttonColor =>
+      brightness == Brightness.dark
+          ? Colors.cyan.withOpacity(0.5)
+          : Colors.blue;
 }
